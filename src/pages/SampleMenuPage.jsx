4191108e67cca51b3d54137b2080724a298dd68f
BonyRoy <94554135+BonyRoy@users.noreply.github.com>
2026-09-07 23:28:31 +0530

    commit

--- src/pages/SampleMenuPage.jsx
+++ src/pages/SampleMenuPage.jsx
@@ -1,6 +1,6 @@
 import MenuView from "../components/menu/MenuView";
 import menuData from "../data/menu.json";
-import sampleLogo from "../assets/hero.png";
+import sampleLogo from "../assets/chilli-logo.png";
 
 const SAMPLE_HERO =
   "https://images.unsplash.com/photo-1585937421612-70a008356fbe?auto=format&fit=crop&w=1920&q=80";
